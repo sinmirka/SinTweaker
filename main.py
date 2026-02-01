3b4e8622f96137d07c4f569caa8e4fb861e4140a
sinmirka <oskar.rayapu@mail.ru>
2026-02-02 00:07:21 +0500

    Added very important file

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
 from pathlib import Path
 
+very_important_file = Path(__file__).parent / "gui" / "very_important_file.jpg" #fucking never delete it
+
 def main():
+    if not very_important_file.exists():
+        return
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
