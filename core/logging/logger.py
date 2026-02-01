c885949667ae8999081d743d4b70d705d2c84a25
sinmirka <oscarka65@gmail.com>
2026-02-01 13:06:17 +0500

    Logging implemented

---
+++ core/logging/logger.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+class Logger:
+    def __init__(self):
+        self.started_at = datetime.now().strftime("%H:%M:%S")
+        self.buffer = []
+        
+        self.buffer.append(f"[{self.started_at}] Initialized")
+    
+    def log(self, text: str):
+        time = datetime.now().strftime("%H:%M:%S")
+        line = f"[{time}] {text}"
+        self.buffer.append(line)
+        self.flush()
+    
+    def flush(self) -> str:
+        return "\n".join(self.buffer)
