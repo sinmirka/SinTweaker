2e2204a7f01ad47b3ed0991a391a1a2327dfa654
sinmirka <oskar.rayapu@mail.ru>
2026-02-07 01:33:12 +0500

    Metadata: fixed MetadataWindow class and implemented copy button

--- gui/metadata_window.py
+++ gui/metadata_window.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from PySide6.QtWidgets import QDialog
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
+from PySide6.QtWidgets import QApplication, QMessageBox
 
-class MetadataWindow():
+class MetadataWindow(): # god help this code work
     def __init__(self, text: str, parent=None):
         loader = QUiLoader()
 
@@ -15,27 +15,37 @@ class MetadataWindow():
         self.dialog = loader.load(ui_file, parent)
         ui_file.close()
 
+        if self.dialog is None:
+            raise RuntimeError("Failed to load metadata_win.ui")
+
         self.dialog.textMetadata.setPlainText(text)
 
         self._connect_signals()
 
     def _connect_signals(self):
-        self.ui.btnClose.clicked.connect(self.close)
-        self.ui.btnCopy.clicked.connect(self._copy_to_clipboard)
-
-        # these are not working for now 04.02.2026
-        # logic will be connected soon 
-        self.ui.btnExport.clicked.connect(self._export_metadata)
-        self.ui.btnSelectiveClean.clicked.connect(self._selective_clean)
-        self.ui.btnRefresh.clicked.connect(self._refresh_metadata)
-
-    def _connect_signals(self):
         self.dialog.btnClose.clicked.connect(self.dialog.close)
         self.dialog.btnCopy.clicked.connect(self._copy_to_clipboard)
 
+        # these are not working for now 04.02.2026
+        # logic will be connected soon 
+        self.dialog.btnExport.clicked.connect(self._export_metadata)
+        self.dialog.btnSelectiveClean.clicked.connect(self._selective_clean)
+        self.dialog.btnRefresh.clicked.connect(self._refresh_metadata)
+
     def _copy_to_clipboard(self):
-        clipboard = self.dialog.textMetadata.clipboard()
-        clipboard.setText(self.dialog.textMetadata.toPlainText())
+        metadata = self.dialog.textMetadata
+        if metadata.toPlainText() == "No metadata found.":
+            return
+        QApplication.clipboard().setText(f"{self.dialog.textMetadata.toPlainText()}")
+
+    def _export_metadata(self): #TODO: implement these 3 features
+        pass
+
+    def _selective_clean(self):
+        pass
+
+    def _refresh_metadata(self):
+        pass
 
     def exec(self):
         self.dialog.exec()
