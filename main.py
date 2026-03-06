0ba2fbaf601a0885e61049d47ff5b4239ef2fff6
sinmirka <oskar.rayapu@mail.ru>
2026-03-06 18:27:36 +0500

    Add and install icon

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from gui.main_window import MainWindow
 from pathlib import Path
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.ico"))
+    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
